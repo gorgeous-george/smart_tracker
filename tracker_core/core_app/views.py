a3861c2a8eacaf853960feaaad06dc32e69798bd
gorgeous george <ilikepython.really@gmail.com>
2023-01-21 21:55:24 +0200

    page layout: object list is rendered as table

--- tracker_core/core_app/views.py
+++ tracker_core/core_app/views.py
@@ -21,7 +21,7 @@ class CoreObjectListView(ListView):
     Generic class-based list view for all core objects available.
     """
     model = CoreObject
-    paginate_by = 2
+    paginate_by = 5
     template_name = "coreobject_list.html"
 
     def get_queryset(self):
